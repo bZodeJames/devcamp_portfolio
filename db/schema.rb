e5060a7285f59412c3086bf7f831eb9c78f928f4
James Briggs <james@bzode.com>
2017-10-15 14:47:10 -0600

    Updated Seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171012221236) do
+ActiveRecord::Schema.define(version: 20171013223611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,9 +24,10 @@ ActiveRecord::Schema.define(version: 20171012221236) do
 
   create_table "portfolios", force: :cascade do |t|
     t.string "title"
+    t.string "subtitle"
     t.text "body"
     t.text "main_image"
-    t.string "thumb_text"
+    t.text "thumb_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,7 @@ puts "5 skills created"
 9.times do |portfolio_item|
     Portfolio.create!(
         title: "Portfolio title: #{portfolio_item}",
+        subtitle: "Title",
         body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, 
     eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
     voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque
@@ -29,12 +30,9 @@ puts "5 skills created"
     corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate 
     velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
         main_image: "http://via.placeholder.com/600x400",
-        thumb_text: "http://via.placeholder.com/350x200"
+        thumb_image: "http://via.placeholder.com/350x200"
         )
 end
 puts "9 Portfolio items created"
 
-”http://placehold.it/600x400”
-
-”http://placehold.it/600x400”
 
