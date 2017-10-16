4ad91a872d3bc19194e78e163ff837b56c6526a5
James Briggs <james@bzode.com>
2017-10-16 14:52:15 -0600

    Updated Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  resources :portfolios
-  get 'pages/Home'
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
-  get 'pages/About'
-
-  get 'pages/Contact'
+  
+  get 'about', to: 'pages#About'
+  get 'contact', to: 'pages#Contact'
 
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'pages#Home'
+
 end
