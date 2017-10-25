4ae05037dec6c61afad4c94959b181f4a674072a
James Briggs <james@bzode.com>
2017-10-25 14:37:35 -0600

    Updated link styles and buttons for application layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
-    def login_helper
+    def login_helper style 
         if current_user.is_a?(GuestUser)
-            (link_to "Register", new_user_registration_path) +
-            "<br>".html_safe +
-            (link_to "Login", new_user_session_path)
+            (link_to "Register", new_user_registration_path, class: style ) +
+            " ".html_safe +
+            (link_to "Login", new_user_session_path, class: style )
         else 
-            link_to "Logout", destroy_user_session_path, method: :delete 
+            link_to "Logout", destroy_user_session_path, method: :delete , class: style
         end
     end
 
@@ -16,7 +16,7 @@ module ApplicationHelper
         end
     end
     def copyright_generator
-    BzodeViewTool::Renderer.copyright 'bZode Technologies', "The ceiling can't hold us, so please don't take my content"
+        BzodeViewTool::Renderer.copyright 'bZode Technologies', "The ceiling can't hold us"
     end
 end
 
