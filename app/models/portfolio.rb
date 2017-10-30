cd614a23ba234aa7ef7a2c62dada6f76ced90aea
James Briggs <james@bzode.com>
2017-10-30 14:19:25 -0600

    Updated portfolio to incude attr position

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -11,9 +11,7 @@ class Portfolio < ApplicationRecord
 		self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
 		self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
 	end	
-	# def self.angular
-	#  where(subtitle: 'Angular')
-	# end
-
-	# scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Angular') }
+	def self.by_position
+		order("position ASC")
+	end
 end
