4ad3be8275181b55e0404be3611acdd735dabf1e
James Briggs <james@bzode.com>
2017-11-01 15:10:15 -0600

    Updated technologies nested form, and updated form.

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -16,7 +16,6 @@ class PortfoliosController < ApplicationController
 
 	def new
 		@portfolio_item = Portfolio.new
-    3.times { @portfolio_item.technologies.build }
 	end
 
 #  def angular
@@ -71,7 +70,7 @@ class PortfoliosController < ApplicationController
                                       :body, 
                                       :main_image,
                                       :thumb_image,
-                                      technologies_attributes: [:name])
+                                      technologies_attributes: [:id, :name, :_destroy])
   end
 
   def set_portfolio_item
