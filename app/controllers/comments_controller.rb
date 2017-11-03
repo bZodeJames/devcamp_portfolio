331ad81501a6758c49d42ee2ec45dea64c807603
James Briggs <james@bzode.com>
2017-11-03 11:38:16 -0600

    Wrote coffeescript for blog comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,12 @@
 class CommentsController < ApplicationController
     def create
         @comment = current_user.comments.build(comment_params)
-end
+    end
 
 private
 
-def comment_params
-    params.require(:comment).permit(:comment)
+    def comment_params
+        params.require(:comment).permit(:content)
+    end
+
 end
