838a56b729c35bf35b0125a97165594109f7921c
James Briggs <james@bzode.com>
2017-11-03 15:18:47 -0600

    SQUASHED ALL THE BUGS!!!!

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -13,14 +13,14 @@ module ApplicationCable
     end
 
     def connect
-      self.current_user = find_verfied_user || guest_user
+      self.current_user = find_verified_user || guest_user
       logger.add_tags 'ActionCable', current_user.email
       logger.add_tags 'ActionCable', current_user.id
     end
 
     protected
 
-    def find_verfied_user
+    def find_verified_user
       if verified_user = env['warden'].user
         verified_user
       end
