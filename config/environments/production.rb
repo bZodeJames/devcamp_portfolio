01937081509060375a09c76f7f0d392f047e7406
James Briggs <james@bzode.com>
2017-11-08 10:45:52 -0700

    Updated production.rb with errors and application JS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_cable.allowed_request_origins = ['https://bzodejames.herokuapp.com', 'http://bzodejames.herokuapp.com']
+  config.action_cable.url = "wss://bzodejames.herokuapp.com/cable"
 end
 
-config.action_cable.allowed_request_origins = ['https://bzodejames.herokuapp.com', 'http://bzodejames.herokuapp.com']
-config.action_cable.url = "wss://bzodejames.herokuapp.com/cable"
